7c56d9d8b5d8557a491f160ff184da5f16336e09
jazzymaster1 <141975423+jazzymaster1@users.noreply.github.com>
2023-10-04 17:57:26 -0300

    Update 2-fatorial.c

--- 2-fatorial.c
+++ 2-fatorial.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 
 
-void limpatela(){
+void limpatela(){ //função para limpar o terminal do gnu-linux em C;
         printf("\e[1;1H\e[2J");
-} //função para limpar o terminal do gnu-linux em C;
+} 
 
 
-int main(){ 
-unsigned long long input, fat=1;
-char w; 
+int main(){ // função principal
+unsigned long long input, fat; // variáveis input e fat
+char w; // char para desativar o loop do-while principal
 do{
-        fat=1;
+        fat=1; // para reiniciar o fat em 1 cada vez que o loop ocorrer
         limpatela(); //limpa o terminal;
         printf("Insira um inteiro entre (1 < x < 20):  ");
-        scanf("%ld", &input);
+        scanf("%ld", &input); 
 
-        for(int i=1;i<=input;i++){ //loop
+        for(int i=1;i<=input;i++){ // loop que multiplcica fat por ele mesmo vezes a quantidade de loops i 
+                                   // até input
                 fat=fat*i;
         }
 
         printf("!%llu = %llu\n", input,fat);
         printf("Tentar novamente? 'S' ou 'N': ");
-        scanf(" %c", &w);
+        scanf(" %c", &w); 
 }
 while(w == 's' || w == 'S'); //repetir (do) caso o usuário insira S
 return 0;
