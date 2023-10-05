40fad03a1d7496ab561c44d65ce9fd3abd1d1075
jazzymaster1 <141975423+jazzymaster1@users.noreply.github.com>
2023-10-05 14:44:43 -0300

    Update 5-media.c

--- 5-media.c
+++ 5-media.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void limpatela(){
+void limpatela(){ // funcao pra limpar a tela do console
 printf("\e[1;1H\e[2J");
 }
 
-int main(){
-        double arr[100000], r;
+int main(){ // funcao principal
+        double arr[100000], r; // arr: os números na operação // r: quantidade de números a serem calculados
         int quantos;
 
         limpatela();
@@ -21,19 +21,19 @@ int main(){
         printf("| quantos números ?          |\n");
         printf("+============================+\n");
 
-        for( int i = 0 ; i < quantos ; i++ ){
+        for( int i = 0 ; i < quantos ; i++ ){ // for para pegar o input do usuário
                 scanf(" %lf", &arr[i]);
         }
-        for( int i = 0 ; i < quantos ; i++ ){
+        for( int i = 0 ; i < quantos ; i++ ){ // for para encontrar o resultado antes de dividir pela quantidade
                 r+=arr[i];
         }
-        r=r/quantos;
+        r=r/quantos; // soma dos números, dividida pela quantidade, média;
 
         printf("+----------------------------+\n");
         printf("| Aqui está sua média:       |\n");
         printf("+============================+\n");
 
-        printf("%.2lf\n", r);
+        printf("%.2lf\n", r); // printf da média, em formato de double;
 return 0;
 }
 
